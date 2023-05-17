47c7f31d3fe3b2b81b3664af6e98590f038fa3df
nagata <nagata-m@x1studio.co.jp>
2023-05-17 12:05:28 +0900

    Added log rotation  witness

--- witness/start_witness.py
+++ witness/start_witness.py
@@ -1,6 +1,16 @@
 from witness import Witness
 
 witness = Witness()
+
+# check monitor
 witness.monitor_1_check()
+
+# check log rotation
+witness.log_rotation_check('monitor')
+witness.log_rotation_check('alert')
+
+# check connection error
 witness.connection_check()
+
+# send report
 witness.report()
--- witness/witness.py
+++ witness/witness.py
@@ -9,6 +9,7 @@ class Witness:
     def __init__(self):
         # Webhook URL
         self.__TEAMS_URL = "https://x1studiocojp.webhook.office.com/webhookb2/2359c523-6ff7-4e43-9cce-b924c34b9a1b@3e594155-a0af-40ef-a8f2-dc8ce23f3844/IncomingWebhook/5798f01fbb53408ea38a9651139d18ea/4eb77287-1789-4810-b206-e1c3bd304107"
+        self.__teams_text = ""
         self.__HOSTS_FILES = [
             "..\\hosts_1.json",
             "..\\hosts_2.json",
@@ -25,17 +26,31 @@ class Witness:
         self.__alert_log = "..\\alert\\logs"
         self.__alert_message = ""
 
-        # log rotation
-        self.__monitor_log_rotation_log = "..\\log_rotator\\logs"
-        self.__alert_log_rotation_log = ""
-        self.__log_rotation_message = ""
+        # log rotation - monitor
+        self.__monitor_log_rotation_log = "..\\log_rotator\\logs\\monitor-log_rotator-log.csv"
+        self.__monitor_log_rotation_message = ""
+
+        # log rotation - alert
+        self.__alert_log_rotation_log = "..\\log_rotator\\logs\\alert-log_rotator-log.csv"
+        self.__alert_log_rotation_message = ""
 
         # connection error
         self.__connection_message = ""
 
 
-    def get_last_line(file_name):
-        with open(file_name, 'rb') as f:
+    def report(self):
+        teams_message = pymsteams.connectorcard(self.__TEAMS_URL)
+        teams_message.title("TEST on Development Environment")
+        self.get_teams_text()
+        teams_message.text(self.__teams_text)
+        teams_message.send()
+
+
+    def get_teams_text(self):
+        self.__teams_text = self.__monitor_log_rotation_message + self.__alert_log_rotation_message + self.__connection_message
+
+    def get_last_line(self, file_path):
+        with open(file_path, 'rb') as f:
             f.seek(-2, os.SEEK_END)
             while f.read(1) != b'\n':
                 f.seek(-2, os.SEEK_CUR)
@@ -62,9 +77,9 @@ class Witness:
 
     def monitor_1_check(self):
         # file_name = self.__monitor_log + "\\202305\\" + "20230515_monitor_1_log.csv"
-        file_name = "..\\monitor\\logs\\202305\\20230515_monitor_1_log.csv"
-        print("file_name", file_name)
-        last_line = Witness.get_last_line(file_name)
+        file_path = "..\\monitor\\logs\\202305\\20230515_monitor_1_log.csv"
+        print("file_name", file_path)
+        last_line = self.get_last_line(file_path)
         with open(".\\test.txt", "a") as f:
             f.write(last_line)
         print("last_line", last_line)
@@ -89,12 +104,25 @@ class Witness:
         self.__alert_message = ""
 
 
-    def log_rotation_check(self):
-        self.__log_rotation_message = ""
+    def log_rotation_check(self, app_name):
 
+        if app_name == 'monitor':
+            file_path = self.__monitor_log_rotation_log
+        else:
+            file_path = self.__alert_log_rotation_log
 
-    def report(self):
-        teams_message = pymsteams.connectorcard(self.__TEAMS_URL)
-        teams_message.title("TEST on Development Environment")
-        teams_message.text(self.__connection_message)
-        teams_message.send()
+        last_line = self.get_last_line(file_path)
+        date_time = last_line.split(',')[0].replace('/', '-')
+        time_diff = (datetime.now() - datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')).total_seconds() / 60
+        print("time_diff", time_diff)
+
+        prefix = "Log rotation - " + app_name + ": "
+
+        message = "ok<br>"
+        if time_diff > 70:
+            message = "Warning. 70 minutes have passed since the last log rotation. Log rotation is executed once every 60 minutes.<br>"
+
+        if app_name == 'monitor':
+            self.__monitor_log_rotation_message = prefix + message
+        else:
+            self.__alert_log_rotation_message = prefix + message
